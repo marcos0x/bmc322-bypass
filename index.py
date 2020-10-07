2ae9955e082d59c5773cd151b81add80d75120d4
Marcos Avila <m.avila@globant.com>
2020-10-07 17:00:44 -0300

    First commit

---
+++ index.py
@@ -0,0 +1,47 @@
+import zlib
+import json
+from flask import Flask, request, Response, url_for, render_template, jsonify
+from base64 import urlsafe_b64encode as b64e, urlsafe_b64decode as b64d
+
+app = Flask(__name__)
+
+with app.test_request_context():
+    url_for('static', filename='styles.css')
+
+def obscure(data):
+    return b64e(zlib.compress(str(data), 9))
+
+def unobscure(obscured):
+    return zlib.decompress(b64d(obscured))
+
+def getLink(url, cuil, mail):
+    oCuil = obscure(cuil)
+    oMail = obscure(mail)
+    return url+'?data='+oCuil+'&m='+oMail+'&utm_source=Email&utm_medium=Canales_Adquisicion'
+
+@app.route('/api/links', methods=['POST'])
+def links():
+    # data = request.get_json()
+    data = request.get_json()
+    url = data['url']
+    cuils = data['cuils'].split(',')
+    emails = data['emails'].split(',')
+    index = 0
+    links = []
+    for cuil in cuils:
+        if len(emails) > 1:
+            email = emails[index]
+        else:
+            email = emails[0]
+            pass
+        links.append(getLink(url, cuil, email))
+        index += 1
+        pass
+
+    return jsonify({
+        "links": links
+    })
+
+@app.route('/')
+def index():
+    return render_template('template.html')
---
+++ static/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $('#form').submit(function(){
+    var data = {
+      url: $('#input_url').val(),
+      cuils: $('#input_cuils').val().split('\n').join(','),
+      emails: $('#input_emails').val().split('\n').join(','),
+    }
+    $.ajax({
+      url: '/api/links',
+      type: 'post',
+      dataType: 'json',
+      contentType: 'application/json',
+      data: JSON.stringify(data),
+      success: function(response) {
+        var links = response.links;
+        $('#links').html(links.join('<br><br>'));
+      }
+    })
+    return false;
+  })
+});
